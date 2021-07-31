2153c0da219e314a6a68e5e6d485da53c6d4f982
Frederik Claus <f.v.claus@googlemail.com>
2021-07-31 18:06:24 +0200

    Adds playground

---
+++ website/webpack.config.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    playground: "./playground/index.js",
+  },
+  devtool: 'inline-source-map',
+  // output: {
+  //   filename: "[name].js",
+  //   path: __dirname + "/static/",
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env", "@babel/react"],
+        },
+      },
+    ],
+  },
+  // output: {
+  //   path: path.resolve(__dirname, './dist'),
+  //   filename: 'index_bundle.js',
+  // },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'playground/index.html'
+  })],
+  externals: {
+    clipboard: "ClipboardJS",
+    codemirror: "CodeMirror",
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'static'),
+    setup: function(app, server) {
+      app.get('/lib/:file', function (req, res) {
+        res.sendFile(path.join(__dirname, 'node_modules', 'prettier', req.params.file));
+      })
+    }
+  }
+};
